1453a11ee0178e6c20a3d3b3ceb01fe9e9b0b042
Iryna <iryna.chorna@hotmail.com>
2025-06-02 14:28:45 +0200

    Updated Terminal

--- EmployeesApp.Terminal/Program.cs
+++ EmployeesApp.Terminal/Program.cs
@@ -1,24 +1,40 @@
 ﻿using EmployeesApp.Application.Employees.Services;
-using EmployeesApp.Domain.Entities;
 using EmployeesApp.Infrastructure.Persistance;
 using EmployeesApp.Infrastructure.Persistance.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 
 
 namespace EmployeesApp.Terminal;
 internal class Program
 {
-    static readonly EmployeeService employeeService = new(new EmployeeRepository();
+    // static readonly EmployeeService employeeService = new(new EmployeeRepository();
 
-    
+
     static void Main(string[] args)
     {
-        ListAllEmployees();
-        ListEmployee(562);
+        var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+        string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // Use the connection string
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+        using ApplicationContext context = new(new DbContextOptionsBuilder<ApplicationContext>()
+            .UseSqlServer(connectionString)
+            .Options);
+
+        EmployeeRepository employeeRepository = new(context);
+        EmployeeService employeeService = new(employeeRepository);
+        ListAllEmployees(employeeService);
+        // ListEmployee(562);
     }
 
-    private static void ListAllEmployees()
+    private static void ListAllEmployees(EmployeeService employeeService)
     {
         foreach (var item in employeeService.GetAll())
             Console.WriteLine(item.Name);
@@ -26,19 +42,19 @@ internal class Program
         Console.WriteLine("------------------------------");
     }
 
-    private static void ListEmployee(int employeeID)
-    {
-        Employee? employee;
+    //private static void ListEmployee(int employeeID)
+    //{
+    //    Employee? employee;
 
-        try
-        {
-            employee = employeeService.GetById(employeeID);
-            Console.WriteLine($"{employee?.Name}: {employee?.Email}");
-            Console.WriteLine("------------------------------");
-        }
-        catch (ArgumentException e)
-        {
-            Console.WriteLine($"EXCEPTION: {e.Message}");
-        }
-    }
+    //    try
+    //    {
+    //        employee = employeeService.GetById(employeeID);
+    //        Console.WriteLine($"{employee?.Name}: {employee?.Email}");
+    //        Console.WriteLine("------------------------------");
+    //    }
+    //    catch (ArgumentException e)
+    //    {
+    //        Console.WriteLine($"EXCEPTION: {e.Message}");
+    //    }
+    //}
 }
