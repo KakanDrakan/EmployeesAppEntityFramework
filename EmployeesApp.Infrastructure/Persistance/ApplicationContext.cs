6867c5b1d012b821a77c6673088b9d8c549e4657
KakanDrakan <tobias.lindskog@yahoo.com>
2025-06-02 12:11:46 +0200

    upp t.o.m. steg 5

---
+++ EmployeesApp.Infrastructure/Persistance/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using EmployeesApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Infrastructure.Persistance;
+
+// Denna konstruktor krävs för att konfigurationen i Program.cs ska fungera
+public class ApplicationContext(DbContextOptions<ApplicationContext> options)
+: DbContext(options)
+{
+    // Exponerar våra entiteter via properties av typen DbSet<T>
+    public DbSet<Employee> Employees { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Anropen nedan är exempel på att använda Fluent API (till skillnad från att
+        // använda attribut direkt på entiteterna)
+        // Specificerar vilken datatyp databasen ska använda för en specifik kolumn
+        //modelBuilder.Entity<Order>()
+        //.Property(o => o.PriceTotal)
+        //.HasColumnType(SqlDbType.Money.ToString());
+        // Specificerar data som en specific tabell ska för-populeras med
+        modelBuilder.Entity<Employee>().HasData(
+        new Employee { Id = 1, Name = "Hans", Email = "Stockholm@gmail.com" },
+        new Employee { Id = 2, Name = "Emma", Email = "test@test.com" },
+        new Employee { Id = 3, Name = "hotmail", Email = "hotmail@hotmail.com" });
+    }
+}
